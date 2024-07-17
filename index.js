c22dccadd54faca4518d3d69508a5fbb7516f0b9
chxttp <thanakritpitiviroj@gmail.com>
2024-07-18 05:59:10 +0700

    third

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
 const port = 3001;
 
@@ -9,83 +8,98 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection string
-const mongoURI = 'mongodb+srv://thanakritpitiviroj:Fc5PVmKRdPrzTG97@cluster0.ljitxic.mongodb.net/carDatabase?retryWrites=true&w=majority&appName=Cluster0';
+// MongoDB connection
+const mongoUri = 'mongodb+srv://thanakritpitiviroj:Fc5PVmKRdPrzTG97@cluster0.ljitxic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-// Define Car schema and model
+// Define a schema and model for car data
 const carSchema = new mongoose.Schema({
-  registrationNumber: String,
-  repairItems: [String]
+  registrationNumber: { type: String, required: true, unique: true },
+  repairItems: [{ type: String }],
 });
 
 const Car = mongoose.model('Car', carSchema);
 
-// Routes
+// API endpoints
+
+// Search for cars by registration number
 app.get('/api/search', async (req, res) => {
-  const query = req.query.query;
   try {
-    const cars = await Car.find({ registrationNumber: { $regex: query, $options: 'i' } });
+    const query = req.query.query;
+    const cars = await Car.find({ registrationNumber: new RegExp(query, 'i') });
     res.json(cars.map(car => car.registrationNumber));
-  } catch (error) {
-    res.status(500).send(error);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
+// Get repair items for a specific car
 app.get('/api/car/:car', async (req, res) => {
-  const car = req.params.car;
   try {
-    const carDetails = await Car.findOne({ registrationNumber: car });
-    if (carDetails) {
-      res.json(carDetails.repairItems);
+    const car = await Car.findOne({ registrationNumber: req.params.car });
+    if (car) {
+      res.json(car.repairItems);
     } else {
       res.status(404).send('Car not found');
     }
-  } catch (error) {
-    res.status(500).send(error);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
+// Add a new car
 app.post('/api/car', async (req, res) => {
-  const { registrationNumber, repairItems } = req.body;
-  const newCar = new Car({ registrationNumber, repairItems });
   try {
-    await newCar.save();
+    const { registrationNumber, repairItems } = req.body;
+    const car = new Car({ registrationNumber, repairItems });
+    await car.save();
     res.status(201).send('Car added');
-  } catch (error) {
-    res.status(500).send(error);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
+// Update repair items for a car
 app.put('/api/car/:car', async (req, res) => {
-  const car = req.params.car;
-  const { repairItems } = req.body;
   try {
-    const updatedCar = await Car.findOneAndUpdate({ registrationNumber: car }, { repairItems }, { new: true });
-    if (updatedCar) {
-      res.json(updatedCar);
+    const { repairItems } = req.body;
+    const car = await Car.findOneAndUpdate(
+      { registrationNumber: req.params.car },
+      { repairItems },
+      { new: true }
+    );
+    if (car) {
+      res.send('Car updated');
     } else {
       res.status(404).send('Car not found');
     }
-  } catch (error) {
-    res.status(500).send(error);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
+// Delete a car
 app.delete('/api/car/:car', async (req, res) => {
-  const car = req.params.car;
   try {
-    const deletedCar = await Car.findOneAndDelete({ registrationNumber: car });
-    if (deletedCar) {
+    const car = await Car.findOneAndDelete({ registrationNumber: req.params.car });
+    if (car) {
       res.send('Car deleted');
     } else {
       res.status(404).send('Car not found');
     }
-  } catch (error) {
-    res.status(500).send(error);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
